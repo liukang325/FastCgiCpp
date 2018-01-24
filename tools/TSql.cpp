224ca5565151dc03ce98e004aad76e6fb58fde51
tu <610903118@qq.com>
2018-01-24 17:17:35 +0800

    finished TSql mysql connect func

--- tools/TSql.cpp
+++ tools/TSql.cpp
@@ -1,7 +1,42 @@
-
+#include <iostream>
 #include "TSql.h"
 
 namespace WebTool {
+    
+TMysql::TMysql()
+{
+    m_pMySql = mysql_init(NULL);
+    if(!m_pMySql)
+    {
+        std::cout << "error:" << mysql_error(m_pMySql);
+        exit(1);
+    }
+}
+
+TMysql::~TMysql()
+{
+
+}
+
+int TMysql::connectMySql(const std::string sIp,
+                     unsigned short uiPort,
+                     const std::string sUserName,
+                     const std::string sPassword,
+                     const std::string sDatabase)
+{
+    if(mysql_real_connect(m_pMySql, sIp.c_str(),\
+                sUserName.c_str(),\
+                sPassword.c_str(),\
+                sDatabase.c_str(), uiPort, NULL, 0))
+    {
+        std::cout << "connect mysql succeed!" << std::endl;
+        return MYSQL_SUCCESS;
+    }
+    else
+    {      
+        std::cout << "error:" << mysql_error(m_pMySql);
+    }
+}
 
 } //namespace WebTool
 
@@ -9,7 +44,10 @@ namespace WebTool {
 #ifdef TEST_MAIN
 int main(int args, char* argv[])
 {
-
+    std::cout << "----------TSQL test----------" << std::endl;
+    WebTool::TMysql mysql;
+    mysql.connectMySql("192.168.244.131", 3306, "root",\
+            "123456", "carbarn");
     return 0;
 }
 #endif
--- tools/TSql.h
+++ tools/TSql.h
@@ -21,18 +21,31 @@
 //in /usr/include/mysql/mysql.h
 #include <mysql.h>
 #include <my_global.h>
+#include <stdlib.h>
 
 //sudo apt-get install libsqlite3-dev
 //in /usr/include/sqlite3.h
 #include <sqlite3.h>
-
+#include <string.h>
 namespace WebTool {
+
+// 错误码  
+enum  
+{  
+    MYSQL_SUCCESS       = 0,    // 成功  
+    MYSQL_E_DBINIT      = 1,    // 数据库初始化失败  
+    MYSQL_E_CONNECT     = 2,    // 数据库连接失败  
+    MYSQL_E_QUERY       = 3,    // 执行查询语句失败  
+    MYSQL_E_RESULT      = 4,    // 获取结果集出错  
+    MYSQL_E_PARAM       = 5,    // 参数错误  
+    MYSQL_E_MEMALLOC    = 6,    // 内存分配失败  
+};  
 
 class  TSql
 {
 public:
-    TSql();
-    ~TSql();
+    TSql() = default;
+    ~TSql() = default;
 
 
 };
@@ -43,7 +56,24 @@ public:
     TMysql();
     ~TMysql();
 
+    /*
+    *  @brief   连接数据库服务器 
+    *  @param   strHost      [in] - 主机地址 
+    *           nPort        [in] - 端口号 
+    *           strUsername  [in] - 用户名 
+    *           strPassword  [in] - 密码 
+    *           strDatabase  [in] - 数据库 
+    *  @return  错误码 
+    **/
+int connectMySql(const std::string sIp,
+                 unsigned short uiPort,
+                 const std::string sUserName,
+                 const std::string sPassword,
+                 const std::string sDatabase);
 
+
+private:
+    MYSQL* m_pMySql = NULL; // mysql 句柄
 };
 
 class  TSqlite: public TSql
